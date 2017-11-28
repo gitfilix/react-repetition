5be119f0e8d97fb000a9a7eb93a8fe4f73dd3c42
gitfilix <runlevel07@gmail.com>
2017-11-28 11:36:21 +0100

    update state

--- coutdowncal/src/App.jsx
+++ coutdowncal/src/App.jsx
@@ -5,10 +5,27 @@ import './app.css';
 
 //ES6 Class App
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        // state : NEVER update state Directly!!
+        this.state = {
+            deadline: '24. December 2017',
+            newDeadline: ''
+        }
+    }
+
+    changeDeadline() {
+        console.log('state: ', this.state);
+        // update state with newDeadline
+        this.setState({deadline: this.state.newDeadline});
+    }
+
+
     render() {
         return(
             <div className="App">
-                <h1 className="app-title">Countdown Calender</h1>
+                <h1 className="app-title">Countdown to {this.state.deadline}</h1>
                 <div>
                     <div className="clock-days">15 days</div>
                     <div className="clock-hours">30 hours</div>
@@ -16,8 +33,11 @@ class App extends Component {
                     <div className="clock-seconds">20 seconds</div>
                 </div>
                 <div>
-                    <input placeholder="new date" />
-                    <button>Submit</button>
+                    <input
+                        placeholder="new date"
+                        onChange={event => this.setState({newDeadline: event.target.value})}
+                        />
+                    <button onClick={()=> this.changeDeadline()}>Submit</button>
                 </div>
             </div>
         )
